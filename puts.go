5d0bcc86d6f97a31126ac91e82cb71e9eff0120c
nultero <nultero@protonmail.com>
2021-07-15 07:55:33 -0500

    moved puts logic to own file; skeleton of validation of paths

--- puts.go
+++ puts.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func dumpPath(p chan string, newpath string) {
+	p <- newpath
+}
+
+func Puts(lb *LogicBus) {
+
+	paths := *&lb.Paths
+	valpaths := make([]string, len(paths))
+
+	for i := range paths {
+		f, err := os.Stat(paths[i])
+		os.IsNotExist(err)
+		if err != nil {
+			fmt.Printf("%s '%s' file does not exist, skipping this path \n", novemIcon, paths[i])
+		} else {
+			fmt.Println(f.ModTime())
+			valpaths = append(valpaths, paths[i])
+		}
+	}
+
+	p := make(chan string)
+	tmp := []string{}
+	for i := range valpaths {
+		go dumpPath(p, valpaths[i])
+		tmp = append(tmp, <-p)
+	}
+
+	fmt.Println(tmp)
+
+}
--- routefuncs.go
+++ routefuncs.go
@@ -4,26 +4,13 @@ import (
 	"fmt"
 )
 
-func dumpPath(p chan string, newpath string) {
-	p <- newpath
-}
-
 func EvalFuncs(lb *LogicBus) {
-
-	p := make(chan string)
-	tmp := []string{}
 
 	switch *&lb.Function {
 	case "puts":
-		paths := *&lb.Paths
-		for i := range paths {
-			go dumpPath(p, paths[i])
-			tmp = append(tmp, <-p)
-		}
+		Puts(*&lb)
 
 	default:
 		fmt.Println("yeet")
 	}
-
-	fmt.Println(tmp)
 }
